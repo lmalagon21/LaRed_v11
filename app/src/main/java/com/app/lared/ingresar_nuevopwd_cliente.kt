e8d282240f5bd1c7ff0d491dfa9d49f3db15770e
Luis Gabriel Malagon <malagondiaz@gmail.com>
2019-07-04 19:00:13 -0500

    Version con controles de la primera pantalla funcionando

--- app/src/main/java/com/app/lared/ingresar_nuevopwd_cliente.kt
+++ app/src/main/java/com/app/lared/ingresar_nuevopwd_cliente.kt
@@ -1,12 +1,19 @@
 package com.app.lared
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ingresar_nuevopwd_cliente : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingresar_nuevopwd_cliente)
+        val cancelar_recuperarpwd : Button = findViewById(R.id.cancelar_newpwd1)
+        cancelar_recuperarpwd.setOnClickListener {
+            val intent = Intent(this, MainActivity :: class.java)
+            startActivity(intent)
+        }
     }
 }
