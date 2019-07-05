298f1c4d15767c90b9949acb5e91ca452d09e7e1
Luis Gabriel Malagon <malagondiaz@gmail.com>
2019-07-05 18:48:27 -0500

    Version con controles de la primera pantalla funcionando

--- app/src/main/java/com/app/lared/registro_negocio1.kt
+++ app/src/main/java/com/app/lared/registro_negocio1.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_registro_negocio1.*
+import kotlinx.android.synthetic.main.activity_registro_negocio2.*
 
 class registro_negocio1 : AppCompatActivity() {
 
@@ -13,8 +16,38 @@ class registro_negocio1 : AppCompatActivity() {
 
         val registroNuevoNegocio : Button = findViewById(R.id.cont_regnegocio2)
         registroNuevoNegocio.setOnClickListener {
-            val intent = Intent(this, registro_negocio3 :: class.java)
-            startActivity(intent)
+            val pregunta1_negocio = this.pregunta1_negocio.getText().toString()
+            val respuesta1_negocio = this.respuesta1_negocio.getText().toString()
+            val pregunta2_negocio = this.pregunta2_negocio.getText().toString()
+            val respuesta2_negocio = this.respuesta2_negocio.getText().toString()
+            if (pregunta1_negocio.equals("")) {
+                Toast.makeText(this, "Debe escoger la pregunta 1",
+                    Toast.LENGTH_LONG).show();
+            } else {
+                if (respuesta1_negocio.equals("")) {
+                    Toast.makeText(
+                        this, "Debe digitar la respuesta 1",
+                        Toast.LENGTH_LONG
+                    ).show();
+                } else {
+                    if (pregunta2_negocio.equals("")) {
+                        Toast.makeText(
+                            this, "Debe escoger la pregunta 2",
+                            Toast.LENGTH_LONG
+                        ).show();
+                    } else {
+                        if (respuesta2_negocio.equals("")) {
+                            Toast.makeText(
+                                this, "Debe digitar la respuesta 2",
+                                Toast.LENGTH_LONG
+                            ).show();
+                        } else {
+                            val intent = Intent(this, registro_negocio3::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                }
+            }
         }
     }
 }
