14dfd02417cb1150cee7a76ace38c333041203bc
Luis Gabriel Malagon <malagondiaz@gmail.com>
2019-07-09 11:51:38 -0500

    Version con controles de la primera pantalla funcionando

--- app/src/main/java/com/app/lared/registro_negocio.kt
+++ app/src/main/java/com/app/lared/registro_negocio.kt
@@ -3,8 +3,11 @@ package com.app.lared
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.Toast
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_registro_negocio.*
 
@@ -14,20 +17,22 @@ class registro_negocio : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_negocio)
 
-        val registroNuevoNegocio : Button = findViewById(R.id.continuar_reg_neg2)
+        val registroNuevoNegocio: Button = findViewById(R.id.continuar_reg_neg2)
         registroNuevoNegocio.setOnClickListener {
             val nombre_negocio = this.nombrenegocio.getText().toString()
-            val tipo_negocio = this.tipo_negocio.getText().toString()
+            val valor_tipo_negocio = this.tipo_negocio.getSelectedItem().toString()
             if (nombre_negocio.equals("")) {
-                Toast.makeText(this, "Debe ingresar el nombre de su negocio",
-                    Toast.LENGTH_LONG).show();
+                Toast.makeText(
+                    this, "Debe ingresar el nombre de su negocio",
+                    Toast.LENGTH_LONG
+                ).show();
             } else {
-                if (tipo_negocio.equals("")) {
+                if (valor_tipo_negocio.equals("Escoja su tipo de negocio")) {
                     Toast.makeText(
                         this, "Debe escoger el tipo de negocio",
                         Toast.LENGTH_LONG
                     ).show();
-                }else {
+                } else {
                     val intent = Intent(this, registro_negocio2::class.java)
                     startActivity(intent)
                 }
